726ccfdd7b0b958e7a8731798494d9916f18bff0
fedora20 <fedora20@example.com>
2015-02-22 23:10:27 +0900

    2015.2.22 add table_info2xls.pl

---
+++ table_info2xls.pl
@@ -0,0 +1,59 @@
+#!/usr/bin/env perl
+
+use DBI;
+use Spreadsheet::WriteExcel;
+
+$ENV{ORACLE_HOME}='/u01/app/oracle/product/11.2.0/xe';
+$ENV{ORACLE_SID}='XE';
+$ENV{NLS_LANG}='JAPANESE_JAPAN.AL32UTF8';
+$ENV{NLS_DATE_FORMAT}='yyyy/mm/dd hh24:mi:ss';
+$user = 'hr';
+$password = 'hr';
+
+$workbook = Spreadsheet::WriteExcel->new('table_info.xls');
+
+$dbh = DBI->connect("dbi:Oracle:$ENV{ORACLE_SID}",$user,$password);
+
+while(<>){
+  chomp();
+  
+$sql = <<EOS;
+select user_tab.column_name,
+       user_tab.data_type,
+       user_tab.data_length,
+       user_col.comments
+from user_tab_cols user_tab, user_col_comments user_col
+where user_tab.table_name = '$_' and
+      user_tab.table_name = user_col.table_name and
+      user_tab.column_name = user_col.column_name
+EOS
+
+  $sth = $dbh->prepare($sql);
+  $sth->execute();
+
+  $worksheet = $workbook->add_worksheet($_);
+  $format = $workbook->add_format();
+
+  #write column name
+  for($j=0; $j<$sth->{NUM_OF_FIELDS}; $j++){
+    $worksheet->write_string(0, $j, $sth->{NAME}->[$j], $format);
+  }
+
+  $i = 1;
+  while(@row=$sth->fetchrow_array()){
+    $j = 0;
+    foreach(@row){
+      $worksheet->write_string($i, $j, $_, $format);
+      #print $worksheet->get_name,",$i,$j,$_\n";
+      $j++;
+    }
+    $i++;
+  }
+
+  $sth->finish();
+  
+}
+
+$dbh->disconnect();
+
+$workbook->close();
